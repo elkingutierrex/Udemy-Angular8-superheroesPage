0659ae7012305c32c6c2837980dc2e8e00759685
elkingutierrex <elkin.gutierrex@gmail.com>
2019-08-03 19:14:23 -0500

    58 - Componente del Heroe

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute  } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -6,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe:any = {};
+
+  constructor( private activatedRoute : ActivatedRoute,
+               private _heroeService: HeroesService) {
+    this.activatedRoute.params.subscribe( params => {
+      this.heroe = this._heroeService.getHeroe(params['id']);
+    })
+  }
 
   ngOnInit() {
   }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +11,8 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
 
-  constructor( private _heroesService : HeroesService) { }
+  constructor( private _heroesService : HeroesService,
+               private router:Router) { }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
@@ -17,7 +20,7 @@ export class HeroesComponent implements OnInit {
   }
 
   verHeroe( idx:number ){
-    console.log(idx)
+    this.router.navigate(['/heroe',idx])
   }
 
 }
